0f0521beba4a828ee5157aee3bef96075ca55cd8
Muzzammilhusen-B <muzammilhusen.b@applocum.org>
2021-09-20 19:06:15 +0530

    css transition on food hovers

--- src/Menu/FoodGrid.js
+++ src/Menu/FoodGrid.js
@@ -22,9 +22,15 @@ export const Food = styled(Title)`
   background-size: cover;
   filter: contrasta(75%);
   padding: 10px;
-  box-shadow: 0px 0px 10px 0px grey;
+  margin-top: 5px;
+  transition-property: box-shadow margin-top filter;
+  transition-duration: 0.1s;
+  box-shadow: 0px 0px 2px 0px grey;
   &:hover {
     cursor: pointer;
-    opacity: 0.7;
+    filter: contrasta(100%);
+    margin-top: 0px;
+    margin-bottom: 5px;
+    box-shadow: 0px 5px 10px 0px grey;
   }
 `;
